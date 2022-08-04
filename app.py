f6c9dbb22b90554b9c81030d3f9429e84a1246a4
Sergio Lucero <sergiolucero@gmail.com>
2022-08-04 19:33:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 import streamlit as st
-
+import geopandas as gp
 import folium, osmnx, OSMPythonTools
+
 
 st.write(folium.__version__)
 st.write(osmnx.__version__)
 st.write(OSMPythonTools.pkgVersion)
+
+url = 's3://quantcldata/varios/openstreetmap.pk')
+odf = gp.read_file(url)
+
+st.write(odf)
