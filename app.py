e298a1c732f95ba9bdf6dcd067dc53ebef684da4
Sergio Lucero <sergiolucero@gmail.com>
2022-08-04 19:39:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import geopandas as gp
 import folium, osmnx, OSMPythonTools
-
+from streamlit_folium import st_folium
 
 st.write(folium.__version__)
 st.write(osmnx.__version__)
@@ -11,3 +11,10 @@ url = 's3://quantcldata/varios/openstreetmap.pk')
 odf = gp.read_file(url)
 
 st.write(odf)
+
+for ciudad, cdf in odf.groupby('ciudad'):
+    cdf['centro']=cdf.geometry.apply(lambda g: g.centroid)
+    centro = cdf.iloc[0]['centro']
+    st.subheader(ciudad)
+    cfm = folium.Map(location=(centro.y, centro.x)  # centroid
+    st_folium(cfm)
